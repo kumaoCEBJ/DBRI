d5db8dd84fbebe66ea00ed01c8e22c40ddb13bdf
kumao <momonnmomomonn@gmail.com>
2026-06-14 14:05:54 +0900

    v1.05

---
+++ js/storage.js
@@ -0,0 +1,44 @@
+﻿
+let localStorageCallback = null;
+export function registerSaveCallback(callback) {
+    localStorageCallback = callback;
+}
+window.addEventListener('beforeunload', async function (e) {
+    if (localStorageCallback) {
+        localStorageCallback();
+    }
+});
+export function saveToLocalStorage(key, data) {
+    try {
+        localStorage.setItem(key, data);
+        console.log(`Data saved to localStorage: ${key}`);
+    } catch (e) {
+        console.error("Failed to save data to localStorage:", e);
+    }
+}
+export function loadFromLocalStorage(key) {
+    try {
+        return localStorage.getItem(key);
+    } catch (e) {
+        console.error("読み込みエラー : ", e);
+        return null;
+    }
+}
+export function showConfirmation(message) {
+    return confirm('現在のデータは全て上書きされます。実行しますか？');
+}
+export function removeDataFromLocalStorage(key) {
+    try {
+        localStorage.removeItem(key);
+    } catch (e) {
+        console.error("削除失敗 : ", e);
+    }
+}
+
+export function clearAllLocalStorage() {
+    try {
+        localStorage.clear();
+    } catch (e) {
+        console.error("削除失敗 : ", e);
+    }
+}
